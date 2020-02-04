eb24651d442341581c6f8e95fa8df1cad4c67a45
gshoop <greysonshoop@gmail.com>
2020-02-03 16:06:51 -0800

    Removed custom class, added namespace ris

--- src/rogue_example.cpp
+++ src/rogue_example.cpp
@@ -5,69 +5,19 @@
 #include <rogue/interfaces/stream/Fifo.h>
 #include <iostream>
 
-class MyCustomMaster : public rogue::interfaces::stream::Master {
-   public:
-
-      // Create a static class creator to return our custom class
-      // wrapped with a shared pointer
-      static std::shared_ptr<MyCustomMaster> create() {
-         static std::shared_ptr<MyCustomMaster> ret =
-            std::make_shared<MyCustomMaster>();
-         return(ret);
-      }
-
-      // Standard class creator which is called by create
-      MyCustomMaster() : rogue::interfaces::stream::Master() { }
-
-      void myFrameGen() {
-         rogue::interfaces::stream::FramePtr frame;
-         rogue::interfaces::stream::FrameIterator it;
-         uint32_t x;
-
-         uint32_t  size;
-         uint8_t * data;
-
-         
-
-         // First request an empty from from the primary slave
-         // The first arg is the size, the second arg is a boolean
-         // indicating if we can allow zero copy buffers, usually set to true
-
-         // Here we request a frame capable of holding 100 bytes
-         frame = reqFrame(100,true);
-
-         // Unlike the python API we must now specify the new payload size
-         frame->setPayload(20);
-
-         // Here we get an iterator to the frame data
-         it = frame->begin();
-
-         // Set an incrementing value to the first 10 locations
-         for (x=0; x < 10; x++) {
-            *it = x;
-            it++;
-         }
-
-         // Use std::copy to copy data from a data buffer
-         // Here we copy 10 bytes starting a the current position of 10
-         // Update the iterator
-         it = std::copy(data, data+10, it);
-
-         //Send frame
-         sendFrame(frame);
-      }
-};
+namespace ris = rogue::interfaces::stream;
 
 //Shared pointer alias
-typedef std::shared_ptr<MyCustomMaster> MyCustomMasterPtr;
+typedef std::shared_ptr<rogue::interfaces::stream::Master> MasterPtr;
+typedef std::shared_ptr<rogue::interfaces::stream::Slave> SlavePtr;
+
 
 int main(int argc, char const *argv[])
 {
     std::cout << "hi" << "\n";
 
-    MyCustomMasterPtr src = MyCustomMaster::create();
-    typedef std::shared_ptr<rogue::interfaces::stream::Slave> SlavePtr;
-    SlavePtr dst = rogue::interfaces::stream::Slave::create();
+    MasterPtr src = ris::Master::create();
+    SlavePtr dst = ris::Slave::create();
 
     *src >> dst;
 
